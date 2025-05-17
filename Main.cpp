2ae650809115059d68983e916628c917db140810
Rishav_Giri <rishavgiri509@gmail.com>
2025-05-17 17:49:40 +0530

    1. made the background and a cheker board pattern. Background can be changed using the <vector<int>> variable named background. and the size of all the images was changed fri=om 37x37 to 50 x50 to better fit the square grid. there is also 100 pixed gap between the screen and the cheker board manually calculated

--- Main.cpp
+++ Main.cpp
@@ -5,20 +5,40 @@
 using namespace std;
 int main(void)
 {
-    // Initialization
-    //--------------------------------------------------------------------------------------
-    vector<string> map;
-    ifstream Map("map.txt");
-    string temp;
     const int screenWidth = 1000;
     const int screenHeight = 900;
     
+    InitWindow(screenWidth, screenHeight, "War-Game");
+    
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    Image Grass1 = LoadImage("img/BigGrass.png");
+    Texture2D grass1 = LoadTextureFromImage(Grass1);
+    
+    Image Grass2 = LoadImage("img/SmlGrass.png");
+    Texture2D grass2 = LoadTextureFromImage(Grass2);
+    
+    vector<string> map;
+    ifstream Map("map.txt");
+    string temp;
+    
+    // Making the Back Ground
+    vector<vector<int>> background;
+    for(int i=0; i < 14; i++ ){
+        vector<int> temp;
+            for(int j=0; j < 16; j++){
+                    temp.push_back((i+j)%2);
+            }
+        background.push_back(temp);
+        }
     while(std::getline(Map,temp)){
         map.push_back(temp);
     }
     Map.close();
+    
+    UnloadImage(Grass1);
+    UnloadImage(Grass2);
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -37,12 +57,12 @@ int main(void)
         BeginDrawing();
 
             ClearBackground(RAYWHITE);
-            for(int i=0; i < map.size(); i++ ){
-                for(int j=0; j < int(map[i].length()); j++){
-                    if(char(map[i][j])==char(*("*"))){
-                        DrawRectangle((j*50)+100, (i*50)+100, 50, 50, BLACK);
-                    }else if((char(map[i][j])== char(*(".")))){
-                        DrawRectangle((j*50)+100, (i*50)+100, 50, 50, RED);
+            for(int i=0; i < 14; i++ ){
+                for(int j=0; j < 16; j++){
+                    if(background[i][j] == 0){
+                        DrawTexture(grass1, (j*50)+100, (i*50)+100, WHITE);
+                    }else if(background[i][j] == 1){
+                        DrawTexture(grass2, (j*50)+100, (i*50)+100, WHITE);
                     }
                 }
             }
